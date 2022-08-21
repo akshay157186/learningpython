09e58336b10204c4d6dd729e1e7de855526cc911
akshay jaiswal <akshay186@gmail.com>
2022-08-21 23:33:44 +1000

    Adding calculator and string arithmetic python programs

---
+++ calculator.py
@@ -0,0 +1,38 @@
+# creation of calculator.py
+
+# selecting 2 numbers
+
+num1 = int(input("Enter 1st number: "))
+num2 = int(input("enter 2nd number: "))
+
+# Asking user what functions do you want to execute
+
+print("what function do you want to use?")
+
+print("for addition enter +"
+      "\n for subtraction enter - "
+      "\n for multiplication enter *"
+      "\n for division enter /"
+      "\n find a remainder enter %"
+      "\n find quotient enter //"
+      "\n find square root of a number enter **")
+ch = input("Enter any of these char for specific operation +,-,*,/,%,//,**")
+
+result = 0
+
+# selecting the option that the user wants to perform
+
+if ch == '+':
+    result = num1 + num2
+elif ch == '-':
+    result = num1 - num2
+elif ch == '*':
+    result = num1 * num2
+elif ch == '/':
+    result = num1 / num2
+elif ch == '%':
+    result = num1 % num2
+elif ch == '//':
+    result = num1 // num2
+elif ch == '**':
+    result = num1 ** num2
---
+++ string_arithmetic_operations.py
@@ -0,0 +1,23 @@
+#entering a string value
+a = input("Enter a string value ")
+
+#echo the string value
+print(f"Your String value is: {a}")
+
+#enter another string value
+b = input("Enter another string value")
+
+#echo the second string value
+print(f"Your String value is: {b}")
+
+#echo a number(preferably less then 10)
+c = input("Enter a number")
+
+#echo the number
+print (f"You entered number: {c}")
+
+#combine the strings
+
+print(f"{a + b}")
+print(f"{a*3}")
+print(f"{b*3}")
