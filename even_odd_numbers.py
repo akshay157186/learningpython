2f735c865e670ee9148ff4e0574e113c271f9b1b
akshay <akshay186@gmail.com>
2022-09-01 16:52:12 +1000

    this file contains program to print X number of even and odd numbers from a given number by the user

---
+++ even_odd_numbers.py
@@ -0,0 +1,13 @@
+a=int(input("enter a number: "))
+print(f"you entered: {a}")
+
+b=int(input("How many numbers do you wish to print: "))
+print(f"printing {b} even numbers after {a}")
+
+num = a
+for x in range(b):
+    while num % 2 == 0:
+#    while num % 2 != 0:
+        num += 1
+    print(num)
+    num += 1
